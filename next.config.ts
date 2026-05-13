e1178ed8661ab3372ea5f677801ae96bafc42e2e
Altun-Kerimli <karimli.altun@gmail.com>
2026-05-13 03:12:04 +0300

    Initial commit for Vercel deployment

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'standalone',
-  typescript: {
-    // !! WARN !!
-    // Dangerously allow production builds to successfully complete even if
-    // your project has type errors.
-    // !! WARN !!
-    ignoreBuildErrors: true,
-  },
   experimental: {
     serverActions: {
       bodySizeLimit: '50mb', // Allows large batch image uploads
